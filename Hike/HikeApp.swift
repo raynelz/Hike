0f68db5b775dd331dc710769d27360869c626dca
Zahar Litvinchuk <97597336+raynelz@users.noreply.github.com>
2023-07-26 18:29:22 +0300

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Захар Литвинчук on 26.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
